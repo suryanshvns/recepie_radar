52320ecac7722a1ef90bd02cb11638af5cf4cf16
suryanshvns <suryanshvns2603@gmail.com>
2024-03-13 18:20:49 +0530

    integration of the authentication part

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,32 +8,55 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> _loginFormKey = GlobalKey();
+
+  String? username, password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: _buildUI(),
+      body: SafeArea(child: _buildUI()),
     );
   }
 
   Widget _buildUI() {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [_title(), _LoginForm()],
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [_image(), _title(), _LoginForm(), _loginButton()],
+      ),
+    );
+  }
+
+  Widget _image() {
+    return Container(
+      width: 200,
+      height: 170,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50.0),
+        image: const DecorationImage(
+          image: AssetImage('assets/login.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 
   Widget _title() {
-    return const Text(
-      "Recipe Book",
-      style: TextStyle(
-        fontSize: 35,
-        color: Colors.deepOrange,
-        fontWeight: FontWeight.bold,
+    return const Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Text(
+        "Recipe Book",
+        style: TextStyle(
+          fontSize: 35,
+          color: Colors.deepOrange,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
@@ -43,18 +66,83 @@ class _LoginPageState extends State<LoginPage> {
         width: MediaQuery.sizeOf(context).width * 0.90,
         height: MediaQuery.sizeOf(context).height * 0.30,
         child: Form(
+          key: _loginFormKey,
           child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(hintText: "Username"),
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: TextFormField(
+                  initialValue: "kminchelle",
+                  onSaved: (value) {
+                    setState(() {
+                      username = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'enter a username';
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: "Password"),
-                )
-              ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: TextFormField(
+                  initialValue: "0lelplR",
+                  obscureText: true,
+                  onSaved: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'please enter password';
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ));
+  }
+
+  Widget _loginButton() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.45,
+        height: MediaQuery.of(context).size.height * 0.06,
+        child: ElevatedButton(
+          onPressed: () {
+            if (_loginFormKey.currentState?.validate() ?? false) {
+              _loginFormKey.currentState?.save();
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepOrange,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
